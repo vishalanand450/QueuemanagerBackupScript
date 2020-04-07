ab49efe7c5751b8e7df56de7e1965d8781ab0e29
Vishal Anand - Network <anand_vishal@network.lilly.com>
2020-04-07 16:36:20 +0530

    added different functions in code

--- QMGR_backup/src/com/QmgrBackup.java
+++ QMGR_backup/src/com/QmgrBackup.java
@@ -1,20 +1,39 @@
 package com;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class QmgrBackup {
-
 	public static void main(String[] args) throws IOException, InterruptedException
 	{
-		ArrayList<String> x = QmgrDisplay();
-		for (int i = 0; i < x.size(); i++) {
-			System.out.println(x.get(i));
+		ArrayList<String> qm_names = QmgrDisplay();
+		ExecuteDmpCommand(qm_names);
+	}
+	public static void ExecuteDmpCommand(ArrayList<String> qms) throws IOException {
+		for (int i = 0; i < qms.size(); i++) {
+			String qm = qms.get(i);
+			String cmd1 = "dmpmqcfg -m "+ qm;
+    		System.out.println("\n\nExecuting command: " + cmd1);
+    		Process p1 = Runtime.getRuntime().exec(cmd1);
+		    BufferedReader reader1 = new BufferedReader(new InputStreamReader(p1.getInputStream()));		                        
+		    String line1;
+		    File f = new File(qm);
+		    f.createNewFile();
+	    	FileWriter fr = new FileWriter(f);
+		    while ((line1 = reader1.readLine()) != null) {
+		    	fr.write(line1);
+		    	fr.write(System.lineSeparator());
+		    }  
+		    fr.close();
+		    System.out.println("The output has been written into the file: " + qm);
 		}
 	}
-	
 	public static ArrayList<String> QmgrDisplay() throws IOException, InterruptedException 
 	{
 	    String cmd = "dspmq";
@@ -26,17 +45,30 @@ public class QmgrBackup {
 	    System.out.println("\n");
 	    
 	    BufferedReader reader1 = new BufferedReader(new InputStreamReader(p1.getInputStream()));
-	    ArrayList<String> arr = new ArrayList<String>();
+	    ArrayList<String> arr1 = new ArrayList<String>();
 	    String line1 ;
 	    while((line1 = reader1.readLine()) != null)
 	    {
-	    	arr.add(line1);
+	    	String command = RegEx(line1);
+	    	arr1.add(command);
+	    	
 	    }
-	    
-	 return arr;   
+	  return arr1;
 		
 	}
 	
+	public static String RegEx(String str)
+	{
+		Pattern QM_PATTERN = Pattern.compile("\\([A-Z0-9]+\\)");
+		Matcher qm_matcher = QM_PATTERN.matcher(str);
+		String qm_value = "";
+		while(qm_matcher.find()) {
+    		String s = qm_matcher.group();
+    		s = s.replace("(","");
+    		qm_value = s.replace(")","");
+		}
+		return qm_value;
+	}
 	
 
 }
