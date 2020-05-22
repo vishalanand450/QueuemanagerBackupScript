51279298b2de7529ed3d5835de124ef31f261103
Vishal Anand - Network <anand_vishal@network.lilly.com>
2020-05-23 00:48:58 +0530

    added archival funnctionality, windows functionality

--- QMGR_backup/src/com/QmgrBackup.java
+++ QMGR_backup/src/com/QmgrBackup.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Properties;
 import java.util.regex.Matcher;
@@ -29,35 +30,41 @@ public class QmgrBackup {
 		System.out.println("\nHostname is " + host);
 		
 		ArrayList<String> qm_names = QmgrDisplay(); //calling QmgrDisplay function
-		
 		for(int x=0 ; x < qm_names.size() ; x++) 
 		{
 			String s = qm_names.get(x);
 			System.out.println(s);
 		}
 		
-//		ArrayList<String> DmpCommand = ExecuteDmpCommand(qm_names) ;  
-//		System.out.println("\nResults-");
-//		for(int x=0 ; x < DmpCommand.size() ; x++) {
-//			String s = DmpCommand.get(x);
-//			System.out.println(s);
-//		}
-		
 		String DmpCommand = ExecuteDmpCommand(qm_names) ;  //calling ExecuteDmpCommand function
 		System.out.println("\nResults-") ;
 		System.out.println(DmpCommand);
 		
-		DeleteOldFile() ;
+		archiveFiles() ;
+		
+//		DeleteOldFile() ;
 		
 		
-//		mailFunction(DmpCommand) ;  //calling mail function
+		mailFunction(DmpCommand) ;  //calling mail function
 	}
 
 	public static ArrayList<String> QmgrDisplay() throws IOException, InterruptedException   //function for dspmq
 	{
-	    String cmd = "dspmq";
-	    System.out.println("\n\nExecuting command: " + cmd);
-	    Process p1 = Runtime.getRuntime().exec(cmd);
+	    
+	    Process p1 = null;
+	    if(CheckWindowOS()) 
+	    {
+	    	String[] wincmd1 = {"cmd.exe", "/c",  "dspmq"};
+	    	p1 = Runtime.getRuntime().exec(wincmd1);
+		    System.out.println("\n\nExecuting command: " + wincmd1[2]);
+	    } 
+	    else 
+	    {
+	    	String cmd = "dspmq";
+	    	p1 = Runtime.getRuntime().exec(cmd);
+		    System.out.println("\n\nExecuting command: " + cmd);
+	    }
+	    
 	    
 	    int result1 = p1.waitFor();	    
 	    System.out.println("Process exit code: " + result1);
@@ -66,11 +73,11 @@ public class QmgrBackup {
 	    
 	    BufferedReader reader1 = new BufferedReader(new InputStreamReader(p1.getInputStream()));
 	    ArrayList<String> arr1 = new ArrayList<String>();
-	    String line1 ;
+	    String line1 = "" ;
 	    while((line1 = reader1.readLine()) != null)
 	    {
-	    	String command = RegEx(line1); //calling RegEx method
-	    	arr1.add(command);
+	    	String queueManagerName = RegEx(line1); //calling RegEx method
+	    	arr1.add(queueManagerName);
 	    	
 	    }
 	  return arr1;
@@ -93,46 +100,98 @@ public class QmgrBackup {
 	public static String ExecuteDmpCommand(ArrayList<String> qms) throws IOException, InterruptedException  //function for dmpmqcfg
 
 	{
-		ArrayList<String> arr2 = new ArrayList<String>();
 		String output = "" ;
 		for (int i = 0; i < qms.size(); i++) 
 		{
-			String qm = qms.get(i);
-			String cmd2 = "dmpmqcfg -m "+ qm + " -a all -o 1line";
-    		System.out.println("\n\nExecuting command: " + cmd2);
-    		Process p2 = Runtime.getRuntime().exec(cmd2);
-		    BufferedReader reader2 = new BufferedReader(new InputStreamReader(p2.getInputStream()));		                        
-		    String line2;
-		    File f = new File("/jackfruit/MQ_installer/QMGR_backup/"+qm+"_"+GetCurrentTimeStamp()+".mqsc" );
-//		    System.out.println(f.getPath());
-		    f.createNewFile();
-	    	FileWriter fr = new FileWriter(f);		    
-		    while ((line2 = reader2.readLine()) != null) 
-		    {
-		    	fr.write(line2);
-		    	fr.write(System.lineSeparator());
-		    }  
-		    fr.close();
-    	    int result2 = p2.waitFor();
-    	    if(result2 ==0) 
-    	    { 
-    	    	String a = qm + " is up. The output has been written into the file: " + qm+"_"+GetCurrentTimeStamp()+".mqsc on path /jackfruit/MQ_installer/QMGR_backup/" ;    		    
-//    		    System.out.println("The output has been written into the file: " + qm+".mqsc");
-        	    System.out.println("Process exit code: " + result2);
-//        	    arr2.add(a) ;
-//        	    output = output +a+ "\n" ;
-        	    output += a+ "\n" ;
-    	    }
-    	    else 
-    	    {
-    	    	String b = qm + " is down. Backup failed." ;
-//    	    	System.out.println(qm + " is down. Backup failed.");
-    	    	System.out.println("Process exit code: " + result2);
-    	    	f.delete(); 
-//    	    	arr2.add(b) ;
-    	    	output += b+ "\n" ;
-    	    	
-    	    }
+			if (CheckWindowOS()) 
+			{
+				String qm = qms.get(i);
+				String[] wincmd1 = {"cmd.exe", "/c", "dmpmqcfg -m "+ qm + " -a all -o 1line"};
+	    		System.out.println("\n\nExecuting command: " + wincmd1[2]);
+	    		Process p2 = Runtime.getRuntime().exec(wincmd1);
+			    BufferedReader reader2 = new BufferedReader(new InputStreamReader(p2.getInputStream()));
+			    BufferedReader stdError = new BufferedReader(new  InputStreamReader(p2.getErrorStream()));
+			    String line2;
+			    File queueManagerLogDir = new File("E:\\home\\mqm\\log\\queueManagerLog");
+			    if (!queueManagerLogDir.exists()) {
+			    	queueManagerLogDir.mkdirs() ;
+			    }
+			    File f = new File("E:\\home\\mqm\\log\\queueManagerLog\\"+qm+"_"+GetCurrentTimeStamp()+".mqsc" );
+//			    System.out.println(f.getPath());
+			    f.createNewFile();
+			    FileWriter fr = new FileWriter(f);		    
+			    while ((line2 = reader2.readLine()) != null) 
+			    {
+			    	fr.write(line2);
+			    	fr.write(System.lineSeparator());
+			    } 
+			    while ((line2 = stdError.readLine()) != null) 
+		        {
+		            System.out.println(line2);
+		        }
+			    fr.close();
+	    	    int result2 = p2.waitFor();
+	    	    
+	    	    if(result2 ==0) 
+	    	    { 
+	    	    	String a = qm + " is up. Backup has been written into the file: " + qm+"_"+GetCurrentTimeStamp()+".mqsc on path "+queueManagerLogDir ;    		    
+	        	    System.out.println("Process exit code: " + result2);
+//	        	    output = output +a+ "\n" ;
+	        	    output += a+ "\n" ;
+	    	    }
+	    	    else 
+	    	    {
+	    	    	String b = qm + " is down. Backup failed." ;
+	    	    	System.out.println("Process exit code: " + result2);
+	    	    	f.delete();
+	    	    	output += b+ "\n" ;
+	    	    	
+	    	    }
+				
+			}
+			
+			else 
+			{
+				String qm = qms.get(i);
+				String cmd2 = "dmpmqcfg -m "+ qm + " -a all -o 1line";
+	    		System.out.println("\n\nExecuting command: " + cmd2);
+	    		Process p2 = Runtime.getRuntime().exec(cmd2);
+			    BufferedReader reader2 = new BufferedReader(new InputStreamReader(p2.getInputStream()));		                        
+			    String line2;
+			    File queueManagerLogDir = new File("/home/mqm/log/queueManagerLog" );
+			    if (!queueManagerLogDir.exists()) {
+			    	queueManagerLogDir.mkdirs() ;
+			    }
+//			    System.out.println(f.getPath());
+			    File f = new File("/home/mqm/log/queueManagerLog/"+qm+"_"+GetCurrentTimeStamp()+".mqsc" );
+			    f.createNewFile();
+			    FileWriter fr = new FileWriter(f);		    
+			    while ((line2 = reader2.readLine()) != null) 
+			    {
+			    	fr.write(line2);
+			    	fr.write(System.lineSeparator());
+			    }  
+			    fr.close();
+	    	    int result2 = p2.waitFor();
+	    	    if(result2 ==0) 
+	    	    { 
+	    	    	String a = qm + " is up. Backup has been written into the file: " + qm+"_"+GetCurrentTimeStamp()+".mqsc on path "+queueManagerLogDir ;    		    
+	        	    System.out.println("Process exit code: " + result2);
+//	        	    output = output +a+ "\n" ;
+	        	    output += a+ "\n" ;
+	    	    }
+	    	    else 
+	    	    {
+	    	    	String b = qm + " is down. Backup failed." ;
+	    	    	System.out.println("Process exit code: " + result2);
+	    	    	f.delete();
+	    	    	output += b+ "\n" ;
+	    	    	
+	    	    }
+				
+			}
+		
+	    	
 		}
 		return output;
 	}
@@ -164,7 +223,7 @@ public class QmgrBackup {
 		String toAddress = "anand_vishal@network.lilly.com" ;
 //		String ccAddress = "" ;
 		String subject = "Queue Manager backup on " +Hostname() ;
-		String message = "Hello Team,\n\nBackup of Queue managers present on " +Hostname()+ " taken successfully on path /jackfruit/MQ_installer/QMGR_backup/. \n\nResults:-\n\n "+text ;
+		String message = "Hello Team,\n\nQueue manager backup script successfully ran on " +Hostname()+ "\n\nResults:-\n\n"+text+ "\n\nThanks" ;
 		
 		Properties properties = System.getProperties() ;
 		properties.put("mail.smtp.starttls.enable", "true");
@@ -196,8 +255,8 @@ public class QmgrBackup {
 	{
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss"); 
         Date now = new Date();
-        String strDate = dateFormat.format(now);
-        return strDate;
+        String dateTimeStamp = dateFormat.format(now);
+        return dateTimeStamp;
     }
 	
 	public static void DeleteOldFile() 
@@ -206,21 +265,83 @@ public class QmgrBackup {
 		
 		if (f.exists())
 		{ 
-            File[] listFiles = f.listFiles();
+            File[] listOfFiles = f.listFiles();
  
             long daysForDeletion = System.currentTimeMillis() - (1 * 24 * 60 * 60 * 1000);
  
-            for (File listFile: listFiles) 
+            for (File listFile: listOfFiles) 
             { 
                 if (listFile.getName().contains(".mqsc") && listFile.lastModified() < daysForDeletion) 
                 {                	            	
                 	listFile.delete() ;
+                	
                 }               	
                
             }
             
 		}
 		
-	}	
+	}
+	
+	public static void archiveFiles() 
+	{
+		File archiveFolder = null;
+		File sourceFolder = null;
+		long deleteArchiveTime = System.currentTimeMillis() - (5 * 1000);
+		
+		if(CheckWindowOS()) 
+		{
+			archiveFolder = new File("E:\\home\\mqm\\log\\queueManagerLog\\archive");
+		    sourceFolder = new File("E:\\home\\mqm\\log\\queueManagerLog");
+			
+		}
+		
+		else 
+		{
+			archiveFolder = new File("/home/mqm/log/queueManagerLog/archive/");
+		    sourceFolder = new File("/home/mqm/log/queueManagerLog/");
+			
+		}
+
+	    if (!archiveFolder.exists())
+	    {
+	    	archiveFolder.mkdirs();
+	    }
+	    
+	    if (sourceFolder.exists() && sourceFolder.isDirectory())
+	    {
+	        File[] listOfFiles = sourceFolder.listFiles();  
+	        // Get list of the files and iterate over them
+	        
+	        for (File child : listOfFiles )
+	            {
+	        	    if (child.lastModified() < deleteArchiveTime )
+	        	    	
+	        	    	if (CheckWindowOS())
+	        	    	{
+	        	    		child.renameTo(new File(archiveFolder + "\\" + child.getName()));	        	    		
+	        	    	}
+	        	    
+	        	    	else
+	        	    	{
+	        	    		child.renameTo(new File(archiveFolder + "/" + child.getName()));	        	    		
+	        	    	}
+	                
+	            }
+	    }
+	    
+	}
+	
+	public static boolean CheckWindowOS()  // this method returns true if OS is windows
+	{
+		String OS = System.getProperty("os.name") ;
+//		System.out.println("\nOperating system = "+OS);
+		if (OS.contains("nux")) 
+		{
+			return false;
+		}
+		return true;
+		
+	}
 
 }
